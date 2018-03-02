50bc9857a1a24fa26c87759961563ee2f0883e65
Siarhei Dudko <admin@sergdudko.tk>
2018-03-02 17:04:36 +0300

    Добавлено хранилище состояний redux клиенту и серверу. Скорректирована архитектура хранения данных. Для клиента реализованы функции запуск скрипта, запуск команды, реализован функционал сервера для работы с этими функциями. Реализована запись данных в firebase на сервере, есть проблемы с чтением данных.

--- client.js
+++ client.js
@@ -1,10 +1,14 @@
 /* ### Раздел инициализации ### */
 
-var fs=require("fs"),
+const fs=require("fs"),
 colors=require("colors"),
 cryptojs=require("cryptojs"),
-download = require('download-file'),
-user_val = '', 
+download = require("download-file")
+os = require("os"),
+child_process = require("child_process"),
+redux=require("redux"),
+lodash=require("lodash");
+var user_val = '', 
 password_val = '';
 
 getSettings().then(function(value){ 
@@ -43,6 +47,43 @@ getSettings().then(function(value){
 
 
 
+/* ### Хранилище состояний REDUX ### */
+
+function editStore(state = {tasks: {}, complete: [], incomplete:[]}, action){
+	if(action.type === 'ADD_TASK'){
+		var state_new = {tasks: {}, complete: [], incomplete:[]};
+		state_new = lodash.clone(state);
+		state_new.tasks[action.payload.uid] = action.payload.task;
+		return state_new;
+	}
+	if(action.type === 'TASK_COMPLETE'){
+		var state_new = {tasks: {}, complete: [], incomplete:[]};
+		state_new = lodash.clone(state);
+		state_new.complete.push(action.payload.uid);
+		if(clientStorage.getState().incomplete.indexOf(action.payload.uid) !== -1){
+			state_new.incomplete.splice(clientStorage.getState().incomplete.indexOf(action.payload.uid),1);
+		}
+		return state_new;
+	}
+	if(action.type === 'TASK_INCOMPLETE'){
+		var state_new = {tasks: {}, complete: [], incomplete:[]};
+		state_new = lodash.clone(state);
+		state_new.incomplete.push(action.payload.uid);
+		if(clientStorage.getState().complete.indexOf(action.payload.uid) !== -1){
+			state_new.complete.splice(clientStorage.getState().complete.indexOf(action.payload.uid),1);
+		}
+		return state_new;
+	}
+	return state;
+}
+var clientStorage = redux.createStore(editStore);
+
+clientStorage.subscribe(function(){
+	console.log(clientStorage.getState());
+});
+
+
+
 /* ### Раздел функций ### */
 
 //функция чтения файла конфигурации
@@ -75,14 +116,28 @@ function datetime() {
 function listenSocket(socket){
 	socket.on('sendtask', function (data) {
 		console.log(colors.green(datetime() + "Получаю задания!"));
-		for(i=0;i<data.length;i++){
-			switch (data[i].nameTask){
-				case "getFileFromWWW":
-					writeFile(data[i].extLink, data[i].intLink, data[i].fileName);
-					break;
-				default:
-					console.log(data[i]);
-					break;
+		for(var key in data){
+			clientStorage.dispatch({type:'ADD_TASK', payload: {uid:key, task:data[key]}});
+			if(data[key].complete === 'true'){
+				clientStorage.dispatch({type:'TASK_COMPLETE', payload: {uid:key}});
+			} else {
+				clientStorage.dispatch({type:'TASK_INCOMPLETE', payload: {uid:key}});
+			}
+			if((data[key].complete !== 'true') && (clientStorage.getState().complete.indexOf(key) === -1)) {
+				switch (data[key].nameTask){
+					case "getFileFromWWW":
+						writeFile(socket, key, data[key].extLink, data[key].intLink, data[key].fileName);
+						break;
+					case "execFile":
+						execFile(socket, key, data[key].intLink, data[key].fileName, data[key].paramArray);
+						break;
+					case "execCommand":
+						execProcess(socket, key, data[key].execCommand, data[key].platform);
+						break;
+					default:
+						console.log(data[key]);
+						break;
+				}
 			}
 		}
 	});
@@ -91,14 +146,89 @@ function listenSocket(socket){
 	});
 }
 
-//функция записи в файловую систему
-function writeFile(extPath, intPath, fileName){
-	var options = {
-		directory: intPath.replace(/\\/gi, '/'),
-		filename: fileName
-	};
-	download(extPath, options, function(err){
-		if (err) throw err
-		console.log(colors.green(datetime() + "Скачан файл " + extPath + " в директорию " + intPath + fileName + "!"));
-	});
+//функция записи в файловую систему, работаем только с корнем (для win32 диском C)
+function writeFile(socket, uid_val, extPath, intPath, fileName){
+	switch (os.platform()) {
+		case "win32":
+			intPath = 'c:' + intPath;
+			var options = {
+				directory: intPath.replace(/\\/gi, '/'),
+				filename: fileName
+			};
+			download(extPath, options, function(err){
+				if (err) throw err
+				taskOnComplete(socket, uid_val);
+				console.log(colors.green(datetime() + "Скачан файл " + extPath + " в директорию " + intPath + fileName + "!"));
+			});
+			break;
+		case 'linux':
+			var options = {
+				directory: intPath.replace(/\\/gi, '/'),
+				filename: fileName
+			};
+			download(extPath, options, function(err){
+				if (err) throw err
+				taskOnComplete(socket, uid_val);
+				console.log(colors.green(datetime() + "Скачан файл " + extPath + " в директорию " + intPath + fileName + "!"));
+			});
+			break;
+		default:
+			console.log(colors.green(datetime() + "Неизвестный тип платформы " + os.platform() + " !"));
+			break;
+	}
+}
+
+//функция запуска исполняемого файла, работаем только с корнем (для win32 диском C)
+function execFile(socket, uid_val, intPath, fileName, paramArray){
+	switch (os.platform()) {
+		case "win32":
+			if (intPath !== ""){
+				intPath = 'c:' + intPath;
+			}
+			var child = child_process.execFile((intPath.replace(/\\/gi, '/') + fileName), paramArray, (error, stdout, stderr) => {
+				if (error) {
+					throw error;
+				}
+				taskOnComplete(socket, uid_val);
+				console.log(colors.yellow(datetime() + "Запущен файл " + (intPath.replace(/\\/gi, '/') + fileName) + ' ' + paramArray + "!"));
+				console.log(stdout);
+			});
+			break;
+		case 'linux':
+			var child = child_process.execFile((intPath.replace(/\\/gi, '/') + fileName), paramArray, (error, stdout, stderr) => {
+				if (error) {
+					throw error;
+				}
+				taskOnComplete(socket, uid_val);
+				console.log(colors.yellow(datetime() + "Запущен файл " + (intPath.replace(/\\/gi, '/') + fileName) + ' ' + paramArray + "!"));
+				console.log(stdout);
+			}); 
+			break;
+		default:
+			console.log(colors.green(datetime() + "Неизвестный тип платформы " + os.platform() + " !"));
+			break;
+	}
+}
+
+//функция запуска shell-команды
+function execProcess(socket, uid_val, execCommand, platform){
+	if(platform === os.platform()){
+		var child = child_process.exec(execCommand, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`exec error: ${error}`);
+				return;
+			}
+			taskOnComplete(socket, uid_val);
+			console.log(`stdout: ${stdout}`);
+			console.log(`stderr: ${stderr}`);
+		});
+	} else {
+		console.log(colors.green(datetime() + "Команда для другой платформы!"));
+	}
+}
+
+//функция изменения состояния при выполнении таска
+function taskOnComplete(socket, uid_val){
+	clientStorage.dispatch({type:'TASK_COMPLETE', payload: {uid:uid_val}});
+	socket.emit('completetask', { uid: uid_val });
 }
--- server.js
+++ server.js
@@ -1,29 +1,102 @@
 /* ### Раздел переменных ### */
 
-var http=require("http"), 
+const http=require("http"), 
 colors=require("colors"),
 fs=require("fs"),
 cryptojs=require("cryptojs"),
+redux=require("redux"),
+lodash=require("lodash"),
+firebase=require("firebase"),
 port = 80;
 
-/* ### Хранилища состояний ### */
+var config = {
+    apiKey: "AIzaSyCKP0rai4BwWPGuUWAQc8QOtWNHBcBMWsg",
+    authDomain: "syncftp3.firebaseapp.com",
+    databaseURL: "https://syncftp3.firebaseio.com",
+    storageBucket: "syncftp3.appspot.com"
+};
+var firebase_user = 'admin@sergdudko.tk';
+var firebase_pass = '999121';
+firebase.initializeApp(config);
 
-var users = {},
-uids = {}
-tasks = {};
+
+
+/* ### Хранилище состояний REDUX ### */
+
+function editStore(state = {users:{}, uids:{}, tasks: {}}, action){
+	if(action.type === 'ADD_USER'){
+		var state_new = {users:{}, uids:{}, tasks: {}};
+		state_new = lodash.clone(state);
+		state_new.users[action.payload.user] = action.payload.password;
+		return state_new;
+	}
+	if(action.type === 'ADD_UID'){
+		var state_new = {users:{}, uids:{}, tasks: {}};
+		state_new = lodash.clone(state);
+		state_new.uids[action.payload.uid] = action.payload.user;
+		return state_new;
+	}
+	if(action.type === 'ADD_TASK'){
+		var state_new = {users:{}, uids:{}, tasks: {}};
+		state_new = lodash.clone(state);
+		if(typeof(state_new.tasks[action.payload.user]) === 'undefined'){
+			state_new.tasks[action.payload.user] = {};
+		}
+		state_new.tasks[action.payload.user][action.payload.task.uid] = action.payload.task.task;
+		return state_new;
+	}
+	if(action.type === 'COMPLETE_TASK'){
+		var state_new = {users:{}, uids:{}, tasks: {}};
+		state_new = lodash.clone(state);
+		state_new.tasks[action.payload.user][action.payload.task].complete = 'true';
+		return state_new;
+	}
+	if(action.type === 'SYNC'){
+		var state_new = [action.payload];
+		return state_new;
+	}
+	return state;
+}
+var serverStorage = redux.createStore(editStore);
+
+serverStorage.subscribe(function(){
+	AuthUserFirebase(firebase_user, firebase_pass).then(function(value){
+		if(value === 'auth'){
+			SendData(serverStorage.getState());
+		}
+	})
+});
+
+//грузим данные из firebase в redux при старте, если они не null
+AuthUserFirebase(firebase_user, firebase_pass).then(function(value){
+	if(value === 'auth'){
+		GetFirebaseData().then(function(value_child){
+			if(value_child !== null){ console.log(JSON.parse(value_child))
+				//serverStorage.dispatch({type:'SYNC', payload: JSON.parse(value_child)});			
+			}
+		});
+	}
+})
 
 /////////////////////////////////////
 setUser('fitobel.apt01', 'password', cryptojs.Crypto.MD5('12345678'));
-var task = {nameTask:"getFileFromWWW", extLink:"http://vpn.sergdudko.tk/releases/dwpanel-2.2.0-1.noarch.rpm", intLink:"c:\\test\\", fileName: "1.rpm", exec:"false"};
-setTask('fitobel.apt01', task);
+var task1 = {uid:generateUID(), task: {nameTask:'getFileFromWWW', extLink:'http://vpn.sergdudko.tk/releases/dwpanel-2.2.0-1.noarch.rpm', intLink:'/test/', fileName: '1.rpm', exec:'false', complete:'false'}};
+var task2 = {uid:generateUID(), task: {nameTask:'execFile', intLink:'', fileName: 'node', paramArray:['--version'], complete:'false'}};
+var task3 = {uid:generateUID(), task: {nameTask:'execCommand', execCommand:'echo "111"', platform:'win32', complete:'false'}};
+setTask('fitobel.apt01', task1);
+setTask('fitobel.apt01', task2);
+setTask('fitobel.apt01', task3);
+setTask('fitobel.apt03', task3);
 ///////////////////////////////////////////////
+
+
 
 /* ### Раздел функций ### */
 
 //функция проверки имени пользователя и пароля
 function testUser(user_val, password_val){
 	var renameuser = replacer(user_val, true);
-	if (users[renameuser].password === password_val) {
+	if (serverStorage.getState().users[renameuser] === password_val) {
 		return true;
 	} else {
 		return false;
@@ -33,20 +106,13 @@ function testUser(user_val, password_val){
 //функция записи в массив пользователей
 function setUser(user_val, param_val, value_val){
 	var renameuser = replacer(user_val, true);
-	if(typeof(users[renameuser]) === 'undefined'){
-		users[renameuser] = {};
-	}
 	switch (param_val){
 		case 'password':
-			users[renameuser].password = value_val;
+			serverStorage.dispatch({type:'ADD_USER', payload: {user:renameuser, password:value_val}});
 			console.log(colors.green(datetime() + "Регистрация пользователя\nLogin: " + user_val));
 			break;
 		case 'uid':
-			users[renameuser].uid = value_val;
-			if(typeof(uids[value_val]) === 'undefined'){
-				uids[value_val] = {};
-			}
-			uids[value_val].user = replacer(user_val, true);
+			serverStorage.dispatch({type:'ADD_UID', payload: {uid:value_val, user:renameuser}});
 			console.log(colors.green(datetime() + "Установка идентификатора пользователя\nLogin: " + user_val + "\nUID:" + value_val));
 			break;
 		default:
@@ -64,24 +130,66 @@ function datetime() {
 //функция замены "." на "_" и обратно
 function replacer(data_val, value_val){
 	if(value_val){
-		return data_val.replace(/./gi,"_");
+		return data_val.replace(/\./gi,"_");
 	} else {
-		return data_val.replace(/_/gi,".");
+		return data_val.replace(/\_/gi,".");
 	}
 }
 
 //функция добавления задачи
 function setTask(user_val, value_val){
 	var renameuser = replacer(user_val, true);
-	if(typeof(tasks[renameuser]) === 'undefined'){
-		tasks[renameuser] = [];
-	}
-	tasks[renameuser].push(value_val);
+	serverStorage.dispatch({type:'ADD_TASK', payload: {user:renameuser, task:value_val}});
+}
+
+//функция генерации UID
+function generateUID() { 
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); 
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+//функция авторизации в firebase
+function AuthUserFirebase(email,pass){
+	return new Promise(function (resolve){
+		firebase.auth().signInWithEmailAndPassword(email, pass).then(function() {
+			resolve('auth');
+		}).catch(function(error) {
+			resolve('noauth');
+			console.log(colors.red(datetime() + "Проблема с авторизацией в firebase: " + error.message));
+		});
+	});
+};
+
+//функция получения данных из firebase
+function GetFirebaseData(){
+	return new Promise(function (resolve){
+		firebase.database().ref('/').once('value', function(snapshot) {
+			resolve(JSON.stringify(snapshot.val()));
+		});
+	});
+}
+
+//функция записи данных в firebase
+function SendData(DataBody){
+	var tokendata = new Date();
+	tokendatastr = tokendata.toString();
+	firebase.database().ref('/').set(DataBody).then(function(value){
+		console.log(colors.green(datetime() + "Синхронизация с firebase успешна!"));
+	}).catch(function(error){
+		console.log(colors.red(datetime() + "Ошибка записи данных: " + error.message));
+	});
 }
 
 
 
-/* ### Раздел работы с сокетом ### */
+/* ### Раздел работы с SOCKET.IO ### */
 	
 server=http.createServer().listen(port, function() {
 	console.log(colors.gray(datetime() + 'listening on *:' + port));
@@ -97,7 +205,10 @@ io.sockets.on('connection', function (socket) {
 			socket.emit('authorisation', { value: 'true' });
 			setUser(data.user, 'uid', socket.id);
 			console.log(colors.green(datetime() + "Подключение пользователя\nLogin: " + data.user + "\nUID: " + socket.id));
-			socket.emit('sendtask', tasks[replacer(data.user, true)]);
+			socket.emit('sendtask', serverStorage.getState().tasks[replacer(data.user, true)]);
+			socket.on('completetask', function (data) {
+				serverStorage.dispatch({type:'COMPLETE_TASK', payload: {user:serverStorage.getState().uids[socket.id], task:data.uid}});
+			});
 		} else {
 			socket.emit('authorisation', { value: 'false' });
 			console.log(colors.red(datetime() + "Неверный пароль для пользователя\nLogin: " + data.user + "\nUID: " + socket.id));
@@ -105,7 +216,7 @@ io.sockets.on('connection', function (socket) {
 	});
   
 	socket.on('disconnect', function () {
-		console.log(colors.red(datetime() + "Отключение пользователя\nLogin: " + replacer(uids[socket.id].user, false) + "\nUID: " + socket.id));
+		console.log(colors.red(datetime() + "Отключение пользователя\nLogin: " + replacer(serverStorage.getState().uids[socket.id], false) + "\nUID: " + socket.id));
 	});
   
 });
