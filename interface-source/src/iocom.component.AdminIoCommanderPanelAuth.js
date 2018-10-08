935a59feedeba0f38152c7f0f72612e2660f0559
Siarhei Dudko <admin@sergdudko.tk>
2018-10-08 09:32:26 +0300

    fix focus on login form

--- interface-source/src/iocom.component.AdminIoCommanderPanelAuth.js
+++ interface-source/src/iocom.component.AdminIoCommanderPanelAuth.js
@@ -7,6 +7,7 @@
 
 import _ from 'lodash';
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 var store = require('./iocom.store.js');
 import core from './iocom.core.js';
@@ -19,30 +20,14 @@ class AdminIoCommanderPanelAuth extends React.Component{
 		super(props, context);
 		this.state = {
 			login: '',
-			password: '',
+			password: ''
 		}
     }
-	
-	onChangeHandler(e){
-		var self = this;
-		switch(e.target.name){
-			case 'SetParamlogin':
-				if(e.target.value !== self.state.login){
-					self.setState({login: e.target.value});
-				}
-				break;
-			case 'SetParamPassword':
-				if(e.target.value !== self.state.password){
-					self.setState({password: e.target.value});
-				}
-				break;
-		}
-	}
 	
 	onBtnClickHandler(e){
 		var self = this;
 		if(e.target.id === 'submit'){
-			core.initialiseSocket(self.state.login, self.state.password);
+			core.initialiseSocket(ReactDOM.findDOMNode(this.refs.login).value, ReactDOM.findDOMNode(this.refs.password).value);
 		}
 	}
 	
@@ -50,9 +35,9 @@ class AdminIoCommanderPanelAuth extends React.Component{
 		var AdminIoCommanderPanelAuth = new Array;
 		var AdminIoCommanderPanelAuthForm = new Array;
 		//поле ввода логина
-		AdminIoCommanderPanelAuthForm.push(<div className="inputFieldCenter" key={core.generateUID()} >Логин: <input type="text" name="SetParamlogin" autoComplete="username" onChange={this.onChangeHandler.bind(this)} value={this.state.login} /></div>);
+		AdminIoCommanderPanelAuthForm.push(<div className="inputFieldCenter" key={core.generateUID()} >Логин: <input type="text" name="SetParamlogin" autoComplete="username" ref="login" /></div>);
 		//поле ввода пароля
-		AdminIoCommanderPanelAuthForm.push(<div className="inputFieldCenter" key={core.generateUID()} >Пароль: <input type="password" name="SetParamPassword" autoComplete="current-password" onChange={this.onChangeHandler.bind(this)} value={this.state.password} /></div>);
+		AdminIoCommanderPanelAuthForm.push(<div className="inputFieldCenter" key={core.generateUID()} >Пароль: <input type="password" name="SetParamPassword" autoComplete="current-password" ref="password" /></div>);
 		AdminIoCommanderPanelAuth.push(<form key={core.generateUID()} >{AdminIoCommanderPanelAuthForm}</form>);
 		//кнопка входа
 		AdminIoCommanderPanelAuth.push(<div className="inputFieldCenter" key={core.generateUID()} ><button onClick={this.onBtnClickHandler.bind(this)} id='submit'>Войти</button></div>);
